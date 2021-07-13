2149cb29dc337b63edf0a702b0ee367d87efcf0c
titos999 <kxristos994@gmail.com>
2021-07-13 10:24:30 +0300

    Passing Student and Animal Data to Thymeleaf

--- src/main/java/com/titosdev/api/controllers/HomeController.java
+++ src/main/java/com/titosdev/api/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.titosdev.api.controllers;
 
+import com.titosdev.api.services.AnimalService;
+import com.titosdev.api.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +16,20 @@ import java.util.List;
 public class HomeController {
 
     private String message = "Welcome to the front page";
-    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+    private final StudentService studentService;
+    private final AnimalService animalService;
+
+    @Autowired
+    public HomeController(StudentService studentService, AnimalService animalService) {
+        this.studentService = studentService;
+        this.animalService = animalService;
+    }
 
     @GetMapping
     public String getWelcomeMessage(Model model) {
         model.addAttribute("message", message);
-        model.addAttribute("tasks", tasks);
+        model.addAttribute("students", studentService.getStudents());
+        model.addAttribute("animals", animalService.getAnimals());
         return "welcome";
     }
 
