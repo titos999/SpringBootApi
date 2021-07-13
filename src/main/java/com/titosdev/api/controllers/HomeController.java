3a74840971a1d722779cd52d018c3a985bc8dab5
titos999 <kxristos994@gmail.com>
2021-07-13 10:29:44 +0300

    Changed Thymeleaf html page in order to get Student - Animal data

--- src/main/java/com/titosdev/api/controllers/HomeController.java
+++ src/main/java/com/titosdev/api/controllers/HomeController.java
@@ -8,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 @RequestMapping(path = "home")
